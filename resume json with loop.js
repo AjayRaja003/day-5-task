28eea91f3bfb8f1cc54ef06a22bb0b9fdb6e8f06
AjayRaja003 <154897254+AjayRaja003@users.noreply.github.com>
2024-01-07 04:45:22 -0800

    Create resume json with loop.js

---
+++ resume json with loop.js
@@ -0,0 +1,46 @@
+//example resume
+
+var json = [{"name":"Ajay",
+    "mail": "ajay.raja0903@gmail.com"
+},
+{
+    "name":"Ajay"
+    "mail": "ajay.raja0903@gmail.com"
+}];
+
+//--------------------------------------------------------------------------------------------
+
+//for loop
+for(var i = 0; i < json.length; i++) {
+    var obj = json[i];
+
+    console.log(obj.name);
+    console.log(obj.mail);
+
+}
+//-----------------------------------------------------------------------------------------------
+
+//for Each
+json.forEach(function(obj) { console.log(obj.msg); });
+
+//------------------------------------------------------------------------------------------------
+
+//for In
+for (var key in json) {
+if (json.hasOwnProperty(key)) {
+  console.log(json[key].name);
+ 
+}
+}
+
+
+//----------------------------------------------------------------------------------------------
+
+//for Of
+let text = "";
+for (let x of json[key].name) {
+ text += x; 
+}
+ console.log(text);
+
+//---------------------------------------------------------------------------------------------
